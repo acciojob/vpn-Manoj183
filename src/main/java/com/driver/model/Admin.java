08a10c88c570308aef10a132af1076e1cb350aa6
Manoj183 <manojchakri2000@gmail.com>
2024-09-05 23:55:42 +0530

    first solution

--- src/main/java/com/driver/model/Admin.java
+++ src/main/java/com/driver/model/Admin.java
@@ -1,9 +1,11 @@
 package com.driver.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "admin")
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,8 +15,17 @@ public class Admin {
     private String password;
 
     @OneToMany(mappedBy = "admin",cascade = CascadeType.ALL)
-    List<ServiceProvider> serviceProviders;
+    List<ServiceProvider> serviceProviders = new ArrayList<>();
 
+    public Admin() {
+    }
+
+    public Admin(int id, String username, String password, List<ServiceProvider> serviceProviders) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.serviceProviders = serviceProviders;
+    }
     public int getId() {
         return id;
     }
